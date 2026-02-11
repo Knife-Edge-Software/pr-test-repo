b76f2b4539e723fa92d2daebcf77c9cba1738e33
Jim Bourke <jtbourke@gmail.com>
2026-02-11 11:43:01 -0700

    Enhance greet with time-of-day, add Button variants

--- src/utils/greet.js
+++ src/utils/greet.js
@@ -1,12 +1,18 @@
 /**
  * Generate a greeting message.
  * @param {string} name - The name to greet
+ * @param {string} [timeOfDay] - Optional time of day for contextual greeting
  * @returns {string} The greeting
  */
-export function greet(name) {
+export function greet(name, timeOfDay) {
+  if (timeOfDay === 'morning') {
+    return `Good morning, ${name}!`;
+  } else if (timeOfDay === 'evening') {
+    return `Good evening, ${name}!`;
+  }
   return `Hello, ${name}!`;
 }
 
 export function farewell(name) {
-  return `Goodbye, ${name}!`;
+  return `Goodbye, ${name}! See you soon.`;
 }
